4f582e7996699fcac9d50a7027221d4b394311cb
Dr-dyrane <Ogranya.Alex@gmail.com>
2023-05-09 10:51:15 -0700

    Insert in sorted linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -10,27 +10,26 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new_node = malloc(sizeof(*new_node));
-    if (!new_node)
+    listint_t *new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
         return (NULL);
 
     new_node->n = number;
+    new_node->next = NULL;
 
-    if (!*head || (*head)->n >= number)
+    if (*head == NULL || number < (*head)->n)
     {
         new_node->next = *head;
         *head = new_node;
+        return (new_node);
     }
-    else
-    {
-        listint_t *current = *head;
 
-        while (current->next && current->next->n < number)
-            current = current->next;
+    listint_t *current = *head;
+    while (current->next != NULL && current->next->n < number)
+        current = current->next;
 
-        new_node->next = current->next;
-        current->next = new_node;
-    }
+    new_node->next = current->next;
+    current->next = new_node;
 
     return (new_node);
 }
