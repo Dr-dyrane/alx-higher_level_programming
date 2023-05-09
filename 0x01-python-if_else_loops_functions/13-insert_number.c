a69d464bad9cb894198029cc8d9782db25807a32
Dr-dyrane <Ogranya.Alex@gmail.com>
2023-05-09 15:28:49 -0700

    insert node

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -10,29 +10,34 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new_node = NULL;
-	listint_t *current = NULL;
+	listint_t *slowpoke = NULL; /* pointer to current node */
+	listint_t *speedygonzales = NULL; /* pointer to new node */
 
-	current = *head;
+	slowpoke = *head;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	speedygonzales = malloc(sizeof(listint_t)); /* allocate memory for new node */
+	if (speedygonzales == NULL) /* check if allocation fails */
 		return (NULL);
 
-	new_node->n = number;
-	new_node->next = NULL;
+	speedygonzales->n = number; /* assign value to new node */
+	speedygonzales->next = NULL;
 
-	if (*head == NULL || new_node->n < current->n)
+	/* check if head is null or if new node is less than head */
+	if (*head == NULL || speedygonzales->n < slowpoke->n)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		speedygonzales->next = *head; /* make new node point to head */
+		*head = speedygonzales; /* update head to new node */
+		return (speedygonzales); /* return address of new node */
 	}
 
-	while (current->next != NULL && (current->n < new_node->n)
-	       && ((current->next)->n < new_node->n))
-		current = current->next;
-	new_node->next = current->next;
-	current->next = new_node;
-	return (new_node);
+	/* loop through list to find the correct position for new node */
+
+	while (slowpoke->next != NULL && (slowpoke->n < speedygonzales->n)
+	       && ((slowpoke->next)->n < speedygonzales->n))
+		slowpoke = slowpoke->next;
+
+	/* make new node point to the next node after current node */
+	speedygonzales->next = slowpoke->next;
+	slowpoke->next = speedygonzales; /* make current node point to new node */
+	return (speedygonzales); /* return address of new node */
 }
