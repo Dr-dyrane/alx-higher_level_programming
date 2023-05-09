655da189c04c4d1e4a9ed7706f90634797f4b2e9
Dr-dyrane <Ogranya.Alex@gmail.com>
2023-05-09 10:55:17 -0700

    Insert in sorted linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,35 +1,38 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * insert_node - Inserts a number into a sorted singly linked list
+ * insert_node - inserts number into a sorted singly linked list
  *
- * @head: Pointer to the head node of the list
- * @number: Number to be inserted into the list
- *
- * Return: Address of the new node, or NULL on failure
+ * @head: linked list
+ * @number: value to be assigned to new node
+ * Return: address of new node or NULL if fail
  */
 listint_t *insert_node(listint_t **head, int number)
 {
+    listint_t *current = NULL;
     listint_t *new_node = malloc(sizeof(listint_t));
+
     if (new_node == NULL)
         return (NULL);
 
     new_node->n = number;
     new_node->next = NULL;
 
-    if (*head == NULL || number < (*head)->n)
+    current = *head;
+
+    if (*head == NULL || new_node->n < current->n)
     {
         new_node->next = *head;
         *head = new_node;
         return (new_node);
     }
 
-    listint_t *current = *head;
-    while (current->next != NULL && current->next->n < number)
+    while (current->next != NULL && (current->n < new_node->n)
+           && ((current->next)->n < new_node->n))
         current = current->next;
 
     new_node->next = current->next;
     current->next = new_node;
-
     return (new_node);
 }
--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,8 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h> // for size_t
+#include <stddef.h> /* for size_t */
 
+/* Structures */
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -16,33 +17,10 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-/**
- * print_listint - prints all the elements of a listint_t list
- * @h: pointer to the head of the list
- * Return: the number of nodes in the list
- */
+/* Prototypes */
 size_t print_listint(const listint_t *h);
-
-/**
- * add_nodeint_end - adds a new node at the end of a listint_t list
- * @head: pointer to the head of the list
- * @n: integer to be added to the new node
- * Return: the address of the new element, or NULL if it fails
- */
-listint_t *add_nodeint_end(listint_t **head, const int n);
-
-/**
- * free_listint - frees a listint_t list
- * @head: pointer to the head of the list
- */
+listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
-
-/**
- * insert_node - inserts a new node into a sorted listint_t list
- * @head: pointer to the head of the list
- * @number: integer to be added to the new node
- * Return: the address of the new element, or NULL if it fails
- */
 listint_t *insert_node(listint_t **head, int number);
 
 #endif /* LISTS_H */
