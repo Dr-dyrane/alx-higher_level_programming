2511a70316d325bbd250a6a6c995a09d8fe6947d
Dr-dyrane <Ogranya.Alex@gmail.com>
2023-05-11 10:20:00 -0700

    Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    total = 0
+    for i in range(1, len(argv)):
+        total = total + int(argv[i])
+    print("{:d}".format(total))
