441d92d316e33add1a8901d33b7d83a185d4a60d
Dr-dyrane <Ogranya.Alex@gmail.com>
2023-05-11 20:44:25 -0700

    nigerian nepa

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,57 +1,22 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
 from sys import argv
-from calculator_1 import add, substract, multiply, divide
-
-def calculate(a, operator, b):
-
-    # Perform the calculation based on the operator
-
-    if operator == '+':
-
-        return add(a, b)
-
-    elif operator == '-':
-
-        return subtract(a, b)
-
-    elif operator == '*':
-
-        return multiply(a, b)
-
-    elif operator == '/':
-
-        return divide(a, b)
-
-    else:
-
-        return None
-
-if __name__ ==  "__main__":
-
-    # Check the number of arguments
-
+if __name__ == "__main__":
     if len(argv) != 4:
-
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+        if argv[2] == "+":
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        elif argv[2] == "-":
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif argv[2] == "*":
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        elif argv[2] == "/":
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
 
-    # Extract the arguments
-
-    b = int(argv[3])
-    a = int(rgv[1])
-
-    operator = sys.argv[2]
-
-    # Perform the calculation
-
-    result = calculate(a, operator, b)
-
-    if result is None:
-
-        print("Unknown operator. Available operators: +, -, * and /")
-
-        exit(1)
-
-    # Print the result
-
-    print(f"{a} {operator} {b} = {result}")
