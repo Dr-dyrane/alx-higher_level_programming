2f0560332ee308b68ca3d193607f24655f885e0a
Dr-dyrane <Ogranya.Alex@gmail.com>
2023-05-13 15:28:55 -0700

    dyrane is awesome

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -4,24 +4,29 @@
 
 /**
  *print_python_list_info - Print some basic info about Python lists
- *@p: PyObject
+ *@p: PyObject representing the Python list
  *
  *Return: Nothing
  */
 void print_python_list_info(PyObject *p)
 {
+	/*PyObject pointer to store individual list elements */
 	PyObject *object;
+	/*ai for iteration, size to store the size of the list */
 	int ai, size;
-
+	/*Get the size of the Python list */
 	size = Py_SIZE(p);
 
 	printf("[*] Size of the Python List = %d\n", size);
 
 	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
+	/*Iterate through the elements of the list */
 	for (ai = 0; ai < size; ai++)
 	{
+		/*Get the individual list element */
 		object = PyList_GetItem(p, ai);
+		/*Print the element's type */
 		printf("Element %d: %s\n", ai, Py_TYPE(object)->tp_name);
 	}
 }
