d9b8356609011493a7822656dc9678bf3be4b83a
Dr-dyrane <Ogranya.Alex@gmail.com>
2023-05-13 10:14:13 -0700

    Everything can be imported

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,21 +1,25 @@
-#include <Python.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-/**
- * print_python_list_info - prints the information of a python list
- * @p: pointer to the python object
- *
- */
+typedef struct object PyObject;
+
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t size, i;
+	void **ptr;
+	ssize_t size, allocated;
+	ssize_t i;
 	PyObject *obj;
 
-	size = PyList_Size(p);
-	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
-	for (ai = 0; ai < size; ai++)
+	printf("[*] Size of the Python List = %zd\n", *((ssize_t *) p + 1));
+
+	ptr = ((void **) p) + 1;
+	size = *((ssize_t *) p + 1);
+	allocated = *((ssize_t *) p + 2);
+	printf("[*] Allocated = %zd\n", allocated);
+
+	for (i = 0; i < size; i++)
 	{
-		obj = PyList_GetItem(p, ai);
-		printf("Element %ld: %s\n", ai, Py_TYPE(obj)->tp_name);
+		obj = (PyObject *) ptr[i];
+		printf("Element %zd: %s\n", i, ((PyObject *) obj)->ob_type->tp_name);
 	}
 }
