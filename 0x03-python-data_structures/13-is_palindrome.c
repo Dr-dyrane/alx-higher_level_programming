e5c635c1b4615b9c4e4c596508b90b0a8c48ab43
Dr-dyrane <Ogranya.Alex@gmail.com>
2023-05-13 15:21:32 -0700

    dyrane is awesome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -13,26 +13,36 @@ int is_palindrome(listint_t **head)
 	listint_t *start = NULL, *end = NULL;
 	unsigned int fai = 0, size = 0, bai = 0, rest = 0;
 
+	/*Check for invalid input */
 	if (!head)
 		return (0);
 
+	/*Empty list is considered a palindrome */
 	if (!*head)
 		return (1);
 
+	/*Assign the value of head to start pointer */
 	start = *head;
+	/*Calculate the number of elements in the linked list and assign it to size */
 	size = listint_size(start);
+	/*Multiply the size by 2 and assign it to bai (used for backward iteration) */
 	bai = size * 2;
+	/*Subtract 2 from bai and assign it to rest (used for forward iteration) */
 	rest = bai - 2;
+	/*Assign the value of head to end pointer */
 	end = *head;
 
+	/*Compare elements from both ends of the list */
 	for (; fai < rest; fai += 2)
 	{
+		/*If values don't match, it's not a palindrome */
 		if (start[fai].n != end[rest].n)
 			return (0);
 
 		rest -= 2;
 	}
 
+	/*All elements compared, it's a palindrome */
 	return (1);
 }
 
@@ -46,9 +56,13 @@ size_t listint_size(const listint_t *head)
 {
 	int size = 0;
 
+	/*Traverse the linked list and count the elements */
 	while (head)
 	{
+		/*Increase the size counter */
 		++size;
+
+		/*Move to the next node */
 		head = head->next;
 	}
 
