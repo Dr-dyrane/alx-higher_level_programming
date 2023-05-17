21352a856b0383ba41253bdc1a6e4a5f7845dd87
Dr-dyrane <Ogranya.Alex@gmail.com>
2023-05-17 12:21:30 -0700

    dyrane is awesome

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -3,57 +3,58 @@
 
 void print_hexn(const char *str, int n)
 {
-	int i = 0;
+	int jerry = 0;
 
-	for (; i < n - 1; ++i)
-		printf("%02x ", (unsigned char) str[i]);
+	do {
+		printf("%02x ", (unsigned char) str[jerry]);
+		jerry++;
+	} while (jerry < n - 1);
 
-	printf("%02x", str[i]);
+	printf("%02x", str[jerry]);
 }
 
 void print_python_bytes(PyObject *p)
 {
-	PyBytesObject *clone = (PyBytesObject *) p;
-	int calc_bytes, clone_size = 0;
+	PyBytesObject *morty_clone = (PyBytesObject*) p;
+	int morty_bytes, clone_size = 0;
 
 	printf("[.] bytes object info\n");
-	if (PyBytes_Check(clone))
+	if (PyBytes_Check(morty_clone))
 	{
 		clone_size = PyBytes_Size(p);
-		calc_bytes = clone_size + 1;
+		morty_bytes = clone_size + 1;
 
-		if (calc_bytes >= 10)
-			calc_bytes = 10;
+		if (morty_bytes >= 10)
+			morty_bytes = 10;
 
 		printf("  size: %d\n", clone_size);
-		printf("  trying string: %s\n", clone->ob_sval);
-		printf("  first %d bytes: ", calc_bytes);
-		print_hexn(clone->ob_sval, calc_bytes);
+		printf("  trying string: %s\n", morty_clone->ob_sval);
+		printf("  first %d bytes: ", morty_bytes);
+		print_hexn(morty_clone->ob_sval, morty_bytes);
 		printf("\n");
 	}
 	else
-	{
-		printf("  [ERROR] Invalid Bytes Object\n");
-	}
+		printf("[ERROR] Invalid Bytes Object\n");
 }
 
 void print_python_list(PyObject *p)
 {
-	int i = 0, list_len = 0;
-	PyObject *item;
-	PyListObject *clone = (PyListObject *) p;
+	int morty = 0, morty_list_len = 0;
+	PyObject * item;
+	PyListObject *morty_clone = (PyListObject*) p;
 
 	printf("[*] Python list info\n");
-	list_len = PyList_GET_SIZE(p);
-	printf("[*] Size of the Python List = %d\n", list_len);
-	printf("[*] Allocated = %d\n", (int) clone->allocated);
+	morty_list_len = PyList_GET_SIZE(p);
+	printf("[*] Size of the Python List = %d\n", morty_list_len);
+	printf("[*] Allocated = %d\n", (int) morty_clone->allocated);
 
-	for (; i < list_len; ++i)
-	{
-		item = PyList_GET_ITEM(p, i);
-		printf("Element %d: %s\n", i, item->ob_type->tp_name);
+	do {
+		item = PyList_GET_ITEM(p, morty);
+		printf("Element %d: %s\n", morty, item->ob_type->tp_name);
 
 		if (PyBytes_Check(item))
 			print_python_bytes(item);
-	}
+
+		morty++;
+	} while (morty < morty_list_len);
 }
