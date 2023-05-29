22223eb5b79da6f3cb78bd7cc30038e26ac217cd
Dr-dyrane <Ogranya.Alex@gmail.com>
2023-05-29 16:17:12 -0700

    dyrane is awesome

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        count = 0
-        for i in range(x):
-            print(my_list[i], end="")
+    count = 0
+    for ai in range(x):
+        try:
+            print(my_list[ai], end="")
             count += 1
-    except:
-        pass
-    finally:
-        print()
-        return count
+        except IndexError:
+            break
+    print()
+    return count
