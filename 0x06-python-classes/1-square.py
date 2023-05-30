7d5ac786f86fa96c86bd05ccb2d2dec9020a24c2
Dr-dyrane <Ogranya.Alex@gmail.com>
2023-05-30 10:14:58 -0700

    dyrane is awesome

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# 1-square.py
+# Alexander Udeogarahnya <Ogranya.Alex@gmail.com>
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
+
