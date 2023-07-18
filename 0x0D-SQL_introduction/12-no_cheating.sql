e92d0875da2c7041b7f910a49f09aae75aa5fec6
Dr-dyrane <halodyrane@gmail.com>
2023-07-18 11:27:10 -0700

    dyrane is awesome

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- Description: Updates the score of the record with the name "Bob" to 10 in the table second_table of the database hbtn_0c_0.
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
