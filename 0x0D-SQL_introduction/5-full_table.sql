6969482e67478ad1a52df3e9536b90508901fd9e
Dr-dyrane <halodyrane@gmail.com>
2023-07-18 11:07:19 -0700

    dyrane is awesome

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 -- This SQL script prints the full description of the table first_table from the database hbtn_0c_0 in the MySQL server.
-SHOW CREATE TABLE hbtn_0c_0.first_table;
+SHOW CREATE TABLE first_table;
