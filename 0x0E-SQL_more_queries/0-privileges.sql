70525244dff24fe1f09324a0f643609908070794
Dr-dyrane <halodyrane@gmail.com>
2023-07-19 09:48:14 -0700

    dyrane is awesome

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,2 @@
 -- List all privileges for users user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost'; SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost' UNION SHOW GRANTS FOR 'user_0d_2'@'localhost';
