caa52086340cfea799557bc37441142d40964e83
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-14 18:13:37 +0100

    dyrane is awesome

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,25 @@
 #!/usr/bin/node
-function factorial (n) {
-  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+
+/**
+ * Calculates the factorial of a given integer using recursion.
+ * @param {number} n - The integer for which to calculate the factorial.
+ * @returns {number} The factorial of the given integer.
+ */
+function factorial(n) {
+  // Base case: factorial of 0 is 1.
+  if (n === 0 || isNaN(n)) {
+    return 1;
+  } else {
+    // Recursive case: factorial(n) = n * factorial(n - 1)
+    return n * factorial(n - 1);
+  }
 }
 
-console.log(factorial(Number(process.argv[2])));
+// Get the first command-line argument and convert it to a number.
+const inputNumber = Number(process.argv[2]);
+
+// Calculate the factorial of the input number using the factorial function.
+const result = factorial(inputNumber);
+
+// Print the result.
+console.log(result);
