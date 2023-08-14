1fe1b73ad12649eace3f41b74f108e1ef711905e
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-14 17:45:54 +0100

    dyrane is awesome

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,17 +1,44 @@
 #!/usr/bin/node
-const myObject = {
-  type: 'object',
-  value: 12
-};
-console.log(myObject);
 
-myObject.incr = function () {
-  this.value++;
-};
+/**
+ * Represents an object with an incrementable value property.
+ * @typedef {Object} MyObject
+ * @property {string} type - The type of the object.
+ * @property {number} value - The current value of the object.
+ * @property {Function} incr - A function to increment the value.
+ */
 
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
+/**
+ * The main script logic.
+ */
+(function () {
+  // Initialize myObject with an initial type and value.
+  const myObject = {
+    type: 'object',
+    value: 12
+  };
+
+  // Print the initial state of myObject.
+  console.log(myObject);
+
+  /**
+   * Increment the value property of the object.
+   * @method incr
+   * @memberof MyObject
+   */
+  myObject.incr = function () {
+    this.value++;
+  };
+
+  // Call the incr() method to increment the value.
+  myObject.incr();
+
+  // Print the updated state of myObject.
+  console.log(myObject);
+
+  // Call the incr() method again to further increment the value.
+  myObject.incr();
+
+  // Print the final state of myObject after multiple increments.
+  console.log(myObject);
+})();
