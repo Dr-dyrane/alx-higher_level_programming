c32d6df983e443e31e9e2cb15dcc54e96d7b9eee
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-14 18:01:26 +0100

    dyrane is awesome

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,8 +1,27 @@
 #!/usr/bin/node
+
+/**
+ * An example object with a 'type' property and an initial 'value'.
+ * @typedef {Object} MyObject
+ * @property {string} type - The type of the object.
+ * @property {number} value - The value associated with the object.
+ */
+
+/**
+ * Initial object with type 'object' and value '12'.
+ * @type {MyObject}
+ */
 const myObject = {
   type: 'object',
   value: 12
 };
+
+// Display the original object.
 console.log(myObject);
+
+// Modify the value property to 89.
 myObject.value = 89;
+
+// Display the object after modification.
 console.log(myObject);
+
