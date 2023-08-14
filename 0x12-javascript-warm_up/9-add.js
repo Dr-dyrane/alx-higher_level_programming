bb80e1e0039384a16b7823ee2f803c3a31359aff
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-14 18:17:19 +0100

    dyrane is awesome

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,21 @@
 #!/usr/bin/node
+
+/**
+ * Adds two integers and returns the result.
+ * @param {number} a - The first integer.
+ * @param {number} b - The second integer.
+ * @returns {number} The sum of the two integers.
+ */
 function add (a, b) {
   return a + b;
 }
 
-console.log(add(Number(process.argv[2]), Number(process.argv[3])));
+// Get the first and second command-line arguments and convert them to numbers.
+const num1 = Number(process.argv[2]);
+const num2 = Number(process.argv[3]);
+
+// Calculate the sum using the add function.
+const sum = add(num1, num2);
+
+// Print the result.
+console.log(sum);
