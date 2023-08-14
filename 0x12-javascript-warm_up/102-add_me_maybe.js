d94e8dda4a123c52e57ae8f893decf4b00ef1af5
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-14 17:50:21 +0100

    dyrane is awesome

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
+
+/**
+ * Increments a number and calls a given function.
+ * @param {number} number - The number to be incremented.
+ * @param {Function} theFunction - The function to be called.
+ */
 exports.addMeMaybe = function (number, theFunction) {
+  // Increment the number by 1 before passing it to theFunction.
   theFunction(++number);
 };
