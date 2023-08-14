88aab409da3d1f3411c4a8d445caf581ddcb74b9
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-14 17:52:34 +0100

    dyrane is awesome

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,4 +1,13 @@
 #!/usr/bin/node
+
+/**
+ * Executes a given function x times.
+ * @param {number} x - The number of times to execute the function.
+ * @param {Function} theFunction - The function to be executed.
+ */
 exports.callMeMoby = function (x, theFunction) {
-  for (let i = 0; i < x; i++) theFunction();
+  // Loop x times and call the given function in each iteration.
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
 };
