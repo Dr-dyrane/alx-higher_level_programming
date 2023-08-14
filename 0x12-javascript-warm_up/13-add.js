75688085b021c71d7d8cd41001142e6720658b72
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-14 17:57:50 +0100

    dyrane is awesome

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -6,10 +6,7 @@
  * @param {number} b - The second integer.
  * @returns {number} The sum of a and b.
  */
-function add(a, b) {
+export default function add(a, b) {
     return a + b;
   }
-  
-  // Export the add function to make it visible from outside.
-  module.exports = { add };
   
