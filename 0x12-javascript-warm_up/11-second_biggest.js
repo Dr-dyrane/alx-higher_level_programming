8a4472ea3e3a708d4c4bbd2b015f44156e238682
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-14 18:09:33 +0100

    dyrane is awesome

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,28 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
-  console.log(0);
-} else {
-  const args = process.argv
-    .map(Number)
-    .slice(2, process.argv.length)
-    .sort((a, b) => a - b);
-  console.log(args[args.length - 2]);
+
+/**
+ * Searches for the second biggest integer in the list of arguments.
+ * @param {number[]} args - An array of integers.
+ * @returns {number} The second biggest integer, or 0 if not found.
+ */
+function findSecondBiggest(args) {
+  // If there are less than two arguments, return 0.
+  if (args.length < 2) {
+    return 0;
+  }
+
+  // Convert arguments to an array of integers and sort them in ascending order.
+  const integers = args.map(Number).sort((a, b) => a - b);
+
+  // Return the second biggest integer.
+  return integers[integers.length - 2];
 }
+
+// Get the command-line arguments excluding the first two (node and script name).
+const args = process.argv.slice(2);
+
+// Find the second biggest integer among the provided arguments.
+const result = findSecondBiggest(args);
+
+// Print the result.
+console.log(result);
