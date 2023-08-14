c878ef47845f0394532886afd23ba68c4e41ed82
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-14 18:19:56 +0100

    dyrane is awesome

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,26 @@
 #!/usr/bin/node
+
+/**
+ * Prints a square of given size using 'X' characters.
+ * @param {number} size - The size of the square.
+ */
+function printSquare (size) {
+  for (let r = 0; r < size; r++) {
+    let row = '';
+    for (let c = 0; c < size; c++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
+
+// Get the size argument from the command-line.
 const size = Math.floor(Number(process.argv[2]));
+
+// Check if the provided size is a valid number.
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let r = 0; r < size; r++) {
-    let row = '';
-    for (let c = 0; c < size; c++) row += 'X';
-    console.log(row);
-  }
+  // Call the function to print the square.
+  printSquare(size);
 }
