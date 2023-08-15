edbe2343b9d9ecc8174093ffb98ab3c5729be30b
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 18:05:42 +0100

    mod doc

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,10 +1,20 @@
 #!/usr/bin/node
+
+/**
+ * Script that demonstrates incrementing an integer value in an object.
+ * @module 103-object_fct
+ */
+
 const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
 
+/**
+ * Function to increment the integer value in the object.
+ * @function incr
+ */
 myObject.incr = function () {
   this.value++;
 };
