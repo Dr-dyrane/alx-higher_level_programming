1c0738e56bacbf88acd413ab6af09b5d4c125589
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 18:00:49 +0100

    mod doc

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
+
+/**
+ * Script that prints two arguments passed to it in the format: "<arg1> is <arg2>"
+ * @module 4-concat
+ */
+
 console.log(`${process.argv[2]} is ${process.argv[3]}`);
