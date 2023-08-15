86f01f5cb61f743f692be5eef4c562d80705e66a
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 18:03:02 +0100

    mod doc

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
+
+/**
+ * Script that prints three lines using an array and a loop.
+ * @module 6-multi_languages_loop
+ */
+
 for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
   console.log(line);
 }
