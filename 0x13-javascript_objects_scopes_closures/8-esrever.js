e6c9f2bfe0a656a5d108a9439a11c2f85d6af5f1
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 17:36:19 +0100

    dyrane is awesome

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,20 @@
 #!/usr/bin/node
 
+/**
+ * Returns the reversed version of a list.
+ *
+ * This function creates a new list containing the elements of the input list
+ * in reverse order.
+ *
+ * @function
+ * @param {Array} list - The list of elements to be reversed.
+ * @returns {Array} A new list with elements in reverse order.
+ */
 exports.esrever = function (list) {
   const reversedList = [];
   for (let i = list.length - 1; i >= 0; i--) {
     reversedList.push(list[i]);
   }
 
-  return (reversedList);
+  return reversedList;
 };
