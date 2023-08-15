79ea667642e13c94dc8235609f1e8f5e56a07c64
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 16:50:26 +0100

    javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,31 +1,7 @@
 #!/usr/bin/node
+/**
+ * Represents a rectangle
+ */
+class Rectangle {}
 
-// Rectangle class definition
-class Rectangle {
-  // Constructor
-  constructor (width, height) {
-    // Initialize width and height properties
-    this.width = width;
-    this.height = height;
-  }
-
-  // Method to calculate and return the area of the rectangle
-  calculateArea () {
-    return this.width * this.height;
-  }
-
-  // Method to print the dimensions of the rectangle
-  printDimensions () {
-    console.log(`Width: ${this.width}, Height: ${this.height}`);
-  }
-}
-
-// Create an instance of Rectangle
-const r1 = new Rectangle(5, 10);
-
-// Test the methods
-console.log(`Area: ${r1.calculateArea()}`);
-r1.printDimensions();
-
-// Export the Rectangle class
 module.exports = Rectangle;
