7c89b2fc79503f6b0b9d4dc8afaf1b26c902a7b4
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 17:22:32 +0100

    dyrane is awesome

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,34 @@
 #!/usr/bin/node
+
 /**
- * Check the parameters provided
+ * Represents a rectangle class with width and height attributes.
+ *
+ * This class defines a rectangle based on the provided width and height parameters.
+ * If the provided width and height are both positive integers, the attributes are initialized.
+ * Otherwise, an empty object is created.
  */
 class Rectangle {
+  /**
+   * Create a new Rectangle instance.
+   *
+   * @constructor
+   * @param {number} w - The width of the rectangle.
+   * @param {number} h - The height of the rectangle.
+   */
   constructor (w, h) {
+    /**
+     * The width of the rectangle.
+     * @type {number}
+     */
     if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
       this.width = w;
+      /**
+       * The height of the rectangle.
+       * @type {number}
+       */
       this.height = h;
     }
   }
 }
+
 module.exports = Rectangle;
