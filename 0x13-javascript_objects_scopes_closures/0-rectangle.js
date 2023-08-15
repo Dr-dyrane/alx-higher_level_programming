d43351a8e703fcd8678c908071a625bf8a974ece
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 17:10:38 +0100

    dyrane is awesome

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 /**
  * Represents a rectangle
+ * This class defines a rectangle and provides the basic structure
+ * for creating and manipulating rectangles.
  */
 class Rectangle {}
 
