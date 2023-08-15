242cc9f3faa3df4d551cf3d95278203c133c9185
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 17:55:48 +0100

    mod doc

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
+
+/**
+ * Script that prints three lines of different programming language descriptions.
+ * @module 1-multi_languages
+ */
+
+// Use console.log() to print three lines
 console.log('C is fun\nPython is cool\nJavaScript is amazing');
