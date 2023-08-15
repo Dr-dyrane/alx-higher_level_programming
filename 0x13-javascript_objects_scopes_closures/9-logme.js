2b3946b80188cd0b27d9a7b97dde62ecb2ce841a
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 17:37:41 +0100

    dyrane is awesome

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,5 +1,14 @@
 #!/usr/bin/node
 
+/**
+ * Prints the number of arguments already printed and the new argument value.
+ *
+ * This function logs the current count of arguments that have been printed
+ * along with the new argument value provided as input.
+ *
+ * @function
+ * @param {any} item - The argument value to be printed.
+ */
 let counter = 0;
 
 exports.logMe = function count (item) {
