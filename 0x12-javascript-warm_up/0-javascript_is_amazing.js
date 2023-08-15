9bf204c645d6f8949b1cde8b7f61a7b48a3795b4
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 17:53:22 +0100

    dyrane is awesome

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,12 @@
 #!/usr/bin/node
+
+/**
+ * Script that prints the message "JavaScript is amazing".
+ * @module 0-javascript_is_amazing
+ */
+
+// Define a constant variable with the specified message
 const myVar = 'JavaScript is amazing';
+
+// Print the value of the constant variable
 console.log(myVar);
