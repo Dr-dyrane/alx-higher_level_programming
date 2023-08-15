c84b4b87d2eec76e3e52ba421aecdab1b222eee6
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 17:40:12 +0100

    dyrane is awesome

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,6 +1,26 @@
 #!/usr/bin/node
 
+/**
+ * Returns a function that converts a number from base 10 to the specified base.
+ *
+ * This function takes a base as an argument and returns an inner function
+ * that converts a given number from base 10 to the specified base.
+ *
+ * @function
+ * @param {number} base - The target base for conversion.
+ * @returns {function} The inner function that performs the conversion.
+ */
 exports.converter = function (base) {
+  /**
+   * Converts a number from base 10 to the specified base.
+   *
+   * This inner function takes a number as input and returns its
+   * representation in the specified base.
+   *
+   * @function
+   * @param {number} n - The number to be converted.
+   * @returns {string} The converted number in the specified base.
+   */
   function myConverter (n) {
     return n.toString(base);
   }
