5bb5063ad198366c4eeb376ddb11ec4b58059694
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 17:59:38 +0100

    mod doc

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
+
+/**
+ * Script that prints the first argument passed to it.
+ * @module 3-value_argument
+ */
+
 console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
