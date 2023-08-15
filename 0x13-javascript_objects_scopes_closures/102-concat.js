5b7485d4a85b963923ac5a76d35fd4ac216b5c2a
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 17:49:10 +0100

    dyrane is awesome

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,22 +1,36 @@
 #!/usr/bin/node
+
+/**
+ * Script that concatenates the content of two source files into a destination file.
+ * @module 102-concat
+ */
+
 const fs = require('fs');
 
+// Get the file paths of the source files and the destination file from command line arguments
 const fileA = process.argv[2];
 const fileB = process.argv[3];
 const fileC = process.argv[4];
 
+// Check if the source files exist and if the destination file is provided
 if (
   fs.existsSync(fileA) &&
-fs.statSync(fileA).isFile &&
-fs.existsSync(fileB) &&
-fs.statSync(fileB).isFile &&
-fileC !== undefined
+  fs.statSync(fileA).isFile &&
+  fs.existsSync(fileB) &&
+  fs.statSync(fileB).isFile &&
+  fileC !== undefined
 ) {
+  // Read the content of the source files
   const fileAContent = fs.readFileSync(fileA);
   const fileBContent = fs.readFileSync(fileB);
+
+  // Create a write stream for the destination file
   const stream = fs.createWriteStream(fileC);
 
+  // Write the content of the source files to the destination file
   stream.write(fileAContent);
   stream.write(fileBContent);
+
+  // Close the write stream
   stream.end();
 }
