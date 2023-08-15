1d6cdea79c9f22f929210c98b3ed133ef3eb9604
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 17:46:59 +0100

    dyrane is awesome

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,26 @@
 #!/usr/bin/node
+
+/**
+ * Script that computes a dictionary of user ids by occurrence from a given dictionary of occurrences by user id.
+ * @module 101-sorted
+ */
+
+// Import the dictionary of occurrences by user id from the file 101-data.js
 const dict = require('./101-data.js').dict;
 
+// Create a new dictionary for user ids by occurrence
 const newDict = {};
 
-Object.getOwnPropertyNames(dict).forEach(occurences => {
-  if (newDict[dict[occurences]] === undefined) {
-    newDict[dict[occurences]] = [occurences];
+// Iterate through the properties of the dictionary
+Object.getOwnPropertyNames(dict).forEach(occurrences => {
+  // If the occurrence value is not present in the new dictionary, create a new entry
+  if (newDict[dict[occurrences]] === undefined) {
+    newDict[dict[occurrences]] = [occurrences];
   } else {
-    newDict[dict[occurences]].push(occurences);
+    // If the occurrence value is already present, append the user id to the existing entry
+    newDict[dict[occurrences]].push(occurrences);
   }
 });
+
+// Print the new dictionary
 console.log(newDict);
