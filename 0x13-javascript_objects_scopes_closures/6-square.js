84629fc09f97fe1b7eae48e473942cd9d582340b
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 17:33:06 +0100

    dyrane is awesome

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,21 @@
 #!/usr/bin/node
+
 /**
- * Square class that inherits from previous square class
+ * Represents a square class that inherits from the previous square class.
+ *
+ * This class extends the behavior of the previous Square class and adds an
+ * instance method charPrint(c) that prints the square using the character c.
+ * If c is undefined, it uses the character X by default.
  */
 const PrevSquare = require('./5-square');
 
 class Square extends PrevSquare {
+  /**
+   * Print the square using the specified character.
+   *
+   * @method
+   * @param {string} c - The character to be used for printing.
+   */
   charPrint (c) {
     const myChar = c === undefined ? 'X' : c;
     for (let i = 0; i < this.height; i++) {
