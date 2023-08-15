fe97d40d2ed257042b265555f4b16a05a2adfdbb
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 18:04:20 +0100

    mod doc

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
+
+/**
+ * Script that prints "C is fun" x times.
+ * @module 7-multi_c
+ * @param {number} x - The number of times to print the statement.
+ */
+
 const x = Math.floor(Number(process.argv[2]));
+
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
