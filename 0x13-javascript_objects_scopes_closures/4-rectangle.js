5b5d9d3861f4d73deeed5aa92636a0c645c9181a
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 17:27:46 +0100

    dyrane is awesome

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,15 +1,40 @@
 #!/usr/bin/node
+
 /**
- * Check the parameters provided
+ * Represents a rectangle class with width, height, and manipulation methods.
+ *
+ * This class defines a rectangle based on the provided width and height parameters.
+ * If the provided width and height are both positive integers, the attributes are initialized.
+ * The class provides methods to print the rectangle, rotate its dimensions, and double its size.
  */
 class Rectangle {
+  /**
+   * Create a new Rectangle instance.
+   *
+   * @constructor
+   * @param {number} w - The width of the rectangle.
+   * @param {number} h - The height of the rectangle.
+   */
   constructor (w, h) {
+    /**
+     * The width of the rectangle.
+     * @type {number}
+     */
     if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
       this.width = w;
+      /**
+       * The height of the rectangle.
+       * @type {number}
+       */
       this.height = h;
     }
   }
 
+  /**
+   * Print a visual representation of the rectangle.
+   *
+   * This method prints the rectangle using the character 'X'.
+   */
   print () {
     for (let i = 0; i < this.height; i++) {
       let myVar = '';
@@ -23,16 +48,26 @@ class Rectangle {
     }
   }
 
+  /**
+   * Rotate the dimensions of the rectangle.
+   *
+   * This method exchanges the width and height of the rectangle.
+   */
   rotate () {
-    let temp = 0;
-    temp = this.width;
+    const temp = this.width;
     this.width = this.height;
     this.height = temp;
   }
 
+  /**
+   * Double the dimensions of the rectangle.
+   *
+   * This method multiplies the width and height of the rectangle by 2.
+   */
   double () {
     this.width *= 2;
     this.height *= 2;
   }
 }
+
 module.exports = Rectangle;
