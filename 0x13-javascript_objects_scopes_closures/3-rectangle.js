37971c211509357bd482343b239f53fa2e920bcd
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 17:24:58 +0100

    dyrane is awesome

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,15 +1,40 @@
 #!/usr/bin/node
+
 /**
- * Check the parameters provided
+ * Represents a rectangle class with width, height, and printing functionality.
+ *
+ * This class defines a rectangle based on the provided width and height parameters.
+ * If the provided width and height are both positive integers, the attributes are initialized.
+ * The class also provides a method, `print()`, that prints a visual representation of the rectangle using the character 'X'.
  */
 class Rectangle {
+  /**
+   * Create a new Rectangle instance.
+   *
+   * @constructor
+   * @param {number} w - The width of the rectangle.
+   * @param {number} h - The height of the rectangle.
+   */
   constructor (w, h) {
+    /**
+     * The width of the rectangle.
+     * @type {number}
+     */
     if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
       this.width = w;
+      /**
+       * The height of the rectangle.
+       * @type {number}
+       */
       this.height = h;
     }
   }
 
+  /**
+   * Print a visual representation of the rectangle.
+   *
+   * This method prints the rectangle using the character 'X'.
+   */
   print () {
     for (let i = 0; i < this.height; i++) {
       let myVar = '';
@@ -23,4 +48,5 @@ class Rectangle {
     }
   }
 }
+
 module.exports = Rectangle;
