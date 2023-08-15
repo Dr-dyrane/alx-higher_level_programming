4e7cd9f861f2a553a2de8605334b87de7b11d8e0
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 17:44:19 +0100

    dyrane is awesome

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,24 @@
 #!/usr/bin/node
+
+/**
+ * Script that computes a new array by multiplying each value of the imported list by its index.
+ * @module 100-map
+ */
+
+// Import the list from the file 100-data.js
 const list = require('./100-data.js').list;
 
-const newList = list.map((val, idx) => val * idx);
+/**
+ * Computes a new array by multiplying each value of the list by its index.
+ * @param {number} val - The value from the list.
+ * @param {number} idx - The index of the value in the list.
+ * @returns {number} - The product of val and idx.
+ */
+const computeNewValue = (val, idx) => val * idx;
+
+// Use the map function to create the new list
+const newList = list.map(computeNewValue);
+
+// Print both the initial list and the new list
 console.log(list);
 console.log(newList);
