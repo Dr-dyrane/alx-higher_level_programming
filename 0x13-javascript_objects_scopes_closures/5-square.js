57325bfe411b195e0c81777510c4e5914ae1a1bc
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 17:29:06 +0100

    dyrane is awesome

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,10 +1,20 @@
 #!/usr/bin/node
+
 /**
- * Square class that inherits from rectangle class
+ * Represents a square class that inherits from the rectangle class.
+ *
+ * This class defines a square based on the provided size parameter.
+ * It inherits the behavior of the Rectangle class and uses its constructor.
  */
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
+  /**
+   * Create a new Square instance.
+   *
+   * @constructor
+   * @param {number} size - The size of the square's sides.
+   */
   constructor (size) {
     super(size, size);
   }
