443e959f68962fbd58f26d1bc6dcaeb4f1da512e
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 17:34:30 +0100

    dyrane is awesome

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,16 @@
 #!/usr/bin/node
 
+/**
+ * Returns the number of occurrences of a search element in a list.
+ *
+ * This function iterates through the given list and counts the occurrences
+ * of the specified search element.
+ *
+ * @function
+ * @param {Array} list - The list of elements to search through.
+ * @param {*} searchElement - The element to search for in the list.
+ * @returns {number} The number of occurrences of the search element.
+ */
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
 
