b590e47fda189e48c3eb9183375b572f570c1964
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-15 17:58:05 +0100

    mod doc

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
+
+/**
+ * Script that prints a message based on the number of arguments passed.
+ * @module 2-arguments
+ */
+
 const count = process.argv.length;
 console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
