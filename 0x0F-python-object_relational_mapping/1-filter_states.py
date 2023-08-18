abeca3145cd4d3028b50986b98545f6a89ceb198
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-18 15:11:36 +0100

    fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -27,39 +27,39 @@ Output:
 import MySQLdb as db
 from sys import argv
 
-def list_states_with_N(db_username, db_password, db_name):
-    """
-    Retrieves and lists states with names starting with 'N'
-    from the specified database.
-
-    Args:
-        db_username (str): Username for database access.
-        db_password (str): Password for database access.
-        db_name (str): Name of the database to connect to.
-
-    Returns:
-        None
-    """
-    db_connect = db.connect(
-        host="localhost",
-        port=3306,
-        user=db_username,
-        passwd=db_password,
-        db=db_name
-    )
-    db_cursor = db_connect.cursor()
-
-    db_cursor.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
-         ORDER BY states.id ASC"
-    )
-
-    rows_selected = db_cursor.fetchall()
-
-    for row in rows_selected:
-        print(row)
-
 if __name__ == '__main__':
+    def list_states_with_N(db_username, db_password, db_name):
+        """
+        Retrieves and lists states with names starting with 'N'
+        from the specified database.
+
+        Args:
+            db_username (str): Username for database access.
+            db_password (str): Password for database access.
+            db_name (str): Name of the database to connect to.
+
+        Returns:
+            None
+        """
+        db_connect = db.connect(
+            host="localhost",
+            port=3306,
+            user=db_username,
+            passwd=db_password,
+            db=db_name
+        )
+        db_cursor = db_connect.cursor()
+
+        db_cursor.execute(
+            "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+            ORDER BY states.id ASC"
+        )
+
+        rows_selected = db_cursor.fetchall()
+
+        for row in rows_selected:
+            print(row)
+
     if len(argv) != 4:
         print("Usage: ./1-filter_states.py <db_username> "
               "<db_password> <db_name>")
