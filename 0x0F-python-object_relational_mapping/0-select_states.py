c0e479cac0629bc05c44c85592016d5db127813a
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-18 15:07:33 +0100

    fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,35 +9,35 @@ to the console.
 import MySQLdb
 from sys import argv
 
-def list_states(db_username, db_password, db_name):
-    """
-    Retrieves and lists all states from the specified database.
-
-    Args:
-        db_username (str): Username for database access.
-        db_password (str): Password for database access.
-        db_name (str): Name of the database to connect to.
-
-    Returns:
-        None
-    """
-    db_connect = MySQLdb.connect(
-        host="localhost",
-        user=db_username,
-        port=3306,
-        passwd=db_password,
-        db=db_name
-    )
-
-    db_cursor = db_connect.cursor()
-
-    db_cursor.execute("SELECT * FROM states ORDER BY id")
-
-    rows_selected = db_cursor.fetchall()
-
-    for row in rows_selected:
-        print(row)
-
 if __name__ == '__main__':
+    def list_states(db_username, db_password, db_name):
+        """
+        Retrieves and lists all states from the specified database.
+
+        Args:
+            db_username (str): Username for database access.
+            db_password (str): Password for database access.
+            db_name (str): Name of the database to connect to.
+
+        Returns:
+            None
+        """
+        db_connect = MySQLdb.connect(
+            host="localhost",
+            user=db_username,
+            port=3306,
+            passwd=db_password,
+            db=db_name
+        )
+
+        db_cursor = db_connect.cursor()
+
+        db_cursor.execute("SELECT * FROM states ORDER BY id")
+
+        rows_selected = db_cursor.fetchall()
+
+        for row in rows_selected:
+            print(row)
+
     db_username, db_password, db_name = argv[1:4]
     list_states(db_username, db_password, db_name)
