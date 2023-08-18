20c61ed99086e08c589b086634540100dc2c466f
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-18 15:18:38 +0100

    import check

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -28,40 +28,40 @@ Output:
 import MySQLdb as db
 from sys import argv
 
-def filter_states_by_name(db_username, db_password, db_name, name_to_search):
-    """
-    Retrieves and lists states with names matching the provided argument
-    from the specified database.
-
-    Args:
-        db_username (str): Username for database access.
-        db_password (str): Password for database access.
-        db_name (str): Name of the database to connect to.
-        name_to_search (str): The name to search for in the states.
-
-    Returns:
-        None
-    """
-    db_connect = db.connect(
-        host="localhost",
-        port=3306,
-        user=db_username,
-        passwd=db_password,
-        db=db_name
-    )
-    db_cursor = db_connect.cursor()
-
-    db_cursor.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
-         states.id ASC".format(name_to_search)
-    )
-
-    rows_selected = db_cursor.fetchall()
-
-    for row in rows_selected:
-        print(row)
-
 if __name__ == '__main__':
+    def filter_states_by_name(db_username, db_password, db_name, name_to_search):
+        """
+        Retrieves and lists states with names matching the provided argument
+        from the specified database.
+
+        Args:
+            db_username (str): Username for database access.
+            db_password (str): Password for database access.
+            db_name (str): Name of the database to connect to.
+            name_to_search (str): The name to search for in the states.
+
+        Returns:
+            None
+        """
+        db_connect = db.connect(
+            host="localhost",
+            port=3306,
+            user=db_username,
+            passwd=db_password,
+            db=db_name
+        )
+        db_cursor = db_connect.cursor()
+
+        db_cursor.execute(
+            "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
+            states.id ASC".format(name_to_search)
+        )
+
+        rows_selected = db_cursor.fetchall()
+
+        for row in rows_selected:
+            print(row)
+
     if len(argv) != 5:
         print("Usage: ./2-my_filter_states.py <db_username> "
               "<db_password> <db_name> <name_to_search>")
