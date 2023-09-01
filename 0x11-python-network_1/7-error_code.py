98d79f2fd30632abaa4e1b548bf39f9b3b9b9765
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-09-01 12:18:58 +0100

    dyrane is awesome

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,18 +11,13 @@ import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <URL>".format(sys.argv[0]))
-        sys.exit(1)
-
     url = sys.argv[1]
 
     # Send a GET request to the URL
     response = requests.get(url)
-
-    # Display the response body
-    print(response.text)
-
     # Check if the status code is greater than or equal to 400
     if response.status_code >= 400:
         print("Error code:", response.status_code)
+    else:
+        # Display the response body
+        print(response.text)
