f8a671db73891b792d0698926a9da98967c7307e
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-09-26 17:06:46 +0100

    dyrane is awesome

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,32 +2,69 @@
 const request = require('request');
 
 /**
- * Display the status code of a GET request to a URL.
- *
- * @param {string} url - The URL to send the GET request to.
+ * StatusCodeFetcher class for displaying the status code of a GET request to a URL.
  */
-function displayStatusCode (url) {
-  // Send a GET request to the specified URL
-  request(url, (error, response) => {
-    if (error) {
-      // Print the error object if there was an error with the request
-      console.error(error);
+class StatusCodeFetcher {
+  /**
+   * Create a StatusCodeFetcher instance.
+   *
+   * @param {string} url - The URL to send the GET request to.
+   */
+  constructor (url) {
+    /**
+     * The URL to send the GET request to.
+     * @type {string}
+     */
+    this.url = url;
+  }
+
+  /**
+   * Display the status code of the GET request.
+   */
+  displayStatusCode () {
+    // Send a GET request to the specified URL
+    request(this.url, (error, response) => {
+      if (error) {
+        /**
+         * Handle request error.
+         *
+         * @event StatusCodeFetcher#error
+         * @type {Error}
+         */
+        console.error(error);
+        process.exit(1);
+      } else {
+        // Print the status code of the response
+        console.log(`code: ${response.statusCode}`);
+      }
+    });
+  }
+
+  /**
+   * Run the StatusCodeFetcher script.
+   * Checks command-line arguments and displays the status code of the GET request.
+   */
+  static run () {
+    // Check if the user provided a URL as an argument
+    if (process.argv.length !== 3) {
+      /**
+       * Usage error.
+       *
+       * @event StatusCodeFetcher#usageError
+       * @type {Error}
+       */
+      console.error('Usage: ./2-statuscode.js <URL>');
       process.exit(1);
-    } else {
-      // Print the status code of the response
-      console.log(`code: ${response.statusCode}`);
     }
-  });
+
+    // Get the URL from the command line argument
+    const url = process.argv[2];
+
+    // Create an instance of StatusCodeFetcher and display the status code
+    const statusCodeFetcher = new StatusCodeFetcher(url);
+    statusCodeFetcher.displayStatusCode();
+  }
 }
 
-// Check if the user provided a URL as an argument
-if (process.argv.length !== 3) {
-  console.error('Usage: ./2-statuscode.js <URL>');
-  process.exit(1);
-}
-
-// Get the URL from the command line argument
-const url = process.argv[2];
-
-// Call the function to display the status code of the GET request
-displayStatusCode(url);
+// Run the script
+StatusCodeFetcher.run();
