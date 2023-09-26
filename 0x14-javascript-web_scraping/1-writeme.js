306d6ef892218ff011c91b4582ac52b006d62724
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-09-26 17:05:02 +0100

    dyrane is awesome

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,31 +2,74 @@
 const fs = require('fs');
 
 /**
- * Write a string to a file.
- *
- * @param {string} filePath - The path to the file to be written.
- * @param {string} content - The string to write to the file.
+ * FileWriter class for writing a string to a file.
  */
-function writeStringToFile (filePath, content) {
-  // Write the content to the file in utf-8 encoding
-  fs.writeFile(filePath, content, 'utf-8', (err) => {
-    if (err) {
-      // Print the error object if an error occurred during writing
-      console.error(err);
+class FileWriter {
+  /**
+   * Create a FileWriter instance.
+   *
+   * @param {string} filePath - The path to the file to be written.
+   * @param {string} content - The string to write to the file.
+   */
+  constructor (filePath, content) {
+    /**
+     * The path to the file to be written.
+     * @type {string}
+     */
+    this.filePath = filePath;
+
+    /**
+     * The string to write to the file.
+     * @type {string}
+     */
+    this.content = content;
+  }
+
+  /**
+   * Write the string content to the file.
+   */
+  writeStringToFile () {
+    // Write the content to the file in utf-8 encoding
+    fs.writeFile(this.filePath, this.content, 'utf-8', (err) => {
+      if (err) {
+        /**
+         * Handle file write error.
+         *
+         * @event FileWriter#error
+         * @type {Error}
+         */
+        console.error(err);
+        process.exit(1);
+      }
+    });
+  }
+
+  /**
+   * Run the FileWriter script.
+   * Checks command-line arguments and writes the string to the file.
+   */
+  static run () {
+    // Check if the user provided both a file path and content as arguments
+    if (process.argv.length !== 4) {
+      /**
+       * Usage error.
+       *
+       * @event FileWriter#usageError
+       * @type {Error}
+       */
+      console.error('Usage: ./1-writeme.js <file_path> <content>');
       process.exit(1);
     }
-  });
+
+    // Get the file path and content from the command line arguments
+    const filePath = process.argv[2];
+    const content = process.argv[3];
+
+    // Create an instance of FileWriter and write the string to the file
+    const fileWriter = new FileWriter(filePath, content);
+    fileWriter.writeStringToFile();
+  }
 }
 
-// Check if the user provided both a file path and content as arguments
-if (process.argv.length !== 4) {
-  console.error('Usage: ./1-writeme.js <file_path> <content>');
-  process.exit(1);
-}
-
-// Get the file path and content from the command line arguments
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-// Call the function to write the string to the file
-writeStringToFile(filePath, content);
+// Run the script
+FileWriter.run();
