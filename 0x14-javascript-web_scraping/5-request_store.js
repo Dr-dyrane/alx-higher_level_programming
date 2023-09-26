417e095553b543fa190d21cdfde9cb8cac14ba50
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-09-26 16:57:09 +0100

    dyrane is awesome

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,36 +1,30 @@
 #!/usr/bin/node
-const fs = require('fs');
 const request = require('request');
+const fs = require('fs');
 
 /**
  * Get the contents of a webpage and store it in a file.
  *
  * @param {string} url - The URL of the webpage to request.
- * @param {string} filePath - The file path to store the body response.
+ * @param {string} file - The file path to store the body response.
  */
-function requestAndStoreWebpage (url, filePath) {
+function requestAndStoreWebpage (url, file) {
   // Send a GET request to the provided URL
   request(url, (error, response, body) => {
     if (error) {
       // Print the error object if there was an error with the request
       console.error(error);
       process.exit(1);
-    }
-
-    if (response.statusCode === 200) {
+    } else {
       // Write the body response to the specified file
-      fs.writeFile(filePath, body, 'utf-8', (err) => {
+      fs.writeFile(file, body, 'utf8', (err) => {
         if (err) {
           // Print the error object if there was an error while writing
           console.error(err);
           process.exit(1);
         }
-
-        console.log(`Downloaded and saved to ${filePath}`);
+        console.log(`Downloaded and saved to ${file}`);
       });
-    } else {
-      console.error(`Error: ${response.statusCode}`);
-      process.exit(1);
     }
   });
 }
@@ -43,7 +37,7 @@ if (process.argv.length !== 4) {
 
 // Define the URL and file path
 const url = process.argv[2];
-const filePath = process.argv[3];
+const file = process.argv[3];
 
 // Call the function to request and store the webpage
-requestAndStoreWebpage(url, filePath);
+requestAndStoreWebpage(url, file);
