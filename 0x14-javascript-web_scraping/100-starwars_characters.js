0a52ef7ffc32b8ca99c4421e7ad5406b73488a30
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-09-26 17:16:07 +0100

    dyrane is awesome

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,59 +2,113 @@
 const request = require('request');
 
 /**
- * Prints all characters of a Star Wars movie by movie ID.
- *
- * @param {number} movieId - The ID of the Star Wars movie.
+ * StarWarsCharacterPrinter class for printing characters of a Star Wars movie by movie ID.
  */
-function getCharactersByMovieId (movieId) {
-  // Define the Star Wars API endpoint URL
-  const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+class StarWarsCharacterPrinter {
+  /**
+   * Create a StarWarsCharacterPrinter instance.
+   *
+   * @param {number} movieId - The ID of the Star Wars movie.
+   */
+  constructor (movieId) {
+    /**
+     * The ID of the Star Wars movie.
+     * @type {number}
+     */
+    this.movieId = movieId;
+  }
 
-  // Send a GET request to the Star Wars API
-  request(apiUrl, (error, response, body) => {
-    if (error) {
-      // Print the error object if there was an error with the request
-      console.error(error);
-      process.exit(1);
-    }
+  /**
+   * Print all characters of the Star Wars movie.
+   */
+  printCharactersByMovieId () {
+    // Define the Star Wars API endpoint URL
+    const apiUrl = `https://swapi-api.alx-tools.com/api/films/${this.movieId}`;
 
-    if (response.statusCode === 200) {
-      // Parse the JSON response
-      const movieData = JSON.parse(body);
+    // Send a GET request to the Star Wars API
+    request(apiUrl, (error, response, body) => {
+      if (error) {
+        /**
+         * Handle request error.
+         *
+         * @event StarWarsCharacterPrinter#error
+         * @type {Error}
+         */
+        console.error(error);
+        process.exit(1);
+      }
 
-      // Print the characters of the movie one by one
-      movieData.characters.forEach((characterUrl) => {
-        request(characterUrl, (charError, charResponse, charBody) => {
-          if (charError) {
-            // Print the error object if there was an error with character request
-            console.error(charError);
-            process.exit(1);
-          }
+      if (response.statusCode === 200) {
+        // Parse the JSON response
+        const movieData = JSON.parse(body);
 
-          if (charResponse.statusCode === 200) {
-            const characterData = JSON.parse(charBody);
-            console.log(characterData.name);
-          } else {
-            console.error(`Error: ${charResponse.statusCode}`);
-            process.exit(1);
-          }
+        // Print the characters of the movie one by one
+        movieData.characters.forEach((characterUrl) => {
+          request(characterUrl, (charError, charResponse, charBody) => {
+            if (charError) {
+              /**
+               * Handle character request error.
+               *
+               * @event StarWarsCharacterPrinter#error
+               * @type {Error}
+               */
+              console.error(charError);
+              process.exit(1);
+            }
+
+            if (charResponse.statusCode === 200) {
+              const characterData = JSON.parse(charBody);
+              console.log(characterData.name);
+            } else {
+              /**
+               * Handle character request response error.
+               *
+               * @event StarWarsCharacterPrinter#error
+               * @type {Error}
+               */
+              console.error(`Error: ${charResponse.statusCode}`);
+              process.exit(1);
+            }
+          });
         });
-      });
-    } else {
-      console.error(`Error: ${response.statusCode}`);
+      } else {
+        /**
+         * Handle response error.
+         *
+         * @event StarWarsCharacterPrinter#error
+         * @type {Error}
+         */
+        console.error(`Error: ${response.statusCode}`);
+        process.exit(1);
+      }
+    });
+  }
+
+  /**
+   * Run the StarWarsCharacterPrinter script.
+   * Checks command-line arguments and prints characters of the specified Star Wars movie.
+   */
+  static run () {
+    // Check if the user provided the movie ID as an argument
+    if (process.argv.length !== 3 || isNaN(Number(process.argv[2]))) {
+      /**
+       * Usage error.
+       *
+       * @event StarWarsCharacterPrinter#usageError
+       * @type {Error}
+       */
+      console.error('Usage: ./100-starwars_characters.js <movie_id>');
       process.exit(1);
     }
-  });
+
+    // Parse the movie ID from the command line argument
+    const movieId = parseInt(process.argv[2]);
+
+    // Create an instance of StarWarsCharacterPrinter and print characters
+    const characterPrinter = new StarWarsCharacterPrinter(movieId);
+    characterPrinter.printCharactersByMovieId();
+  }
 }
 
-// Check if the user provided the movie ID as an argument
-if (process.argv.length !== 3) {
-  console.error('Usage: ./100-starwars_characters.js <movie_id>');
-  process.exit(1);
-}
-
-// Parse the movie ID from the command line argument
-const movieId = parseInt(process.argv[2]);
-
-// Call the function to get characters by movie ID
-getCharactersByMovieId(movieId);
+// Run the script
+StarWarsCharacterPrinter.run();
