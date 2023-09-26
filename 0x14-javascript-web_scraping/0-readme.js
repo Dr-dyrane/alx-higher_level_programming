b6a794ab7a4c15d843130856f94de8da31aa0f8e
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-09-26 17:01:35 +0100

    dyrane is awesome

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,32 +2,74 @@
 const fs = require('fs');
 
 /**
- * Read and print the content of a file.
- *
- * @param {string} filePath - The path to the file to be read.
+ * FileReader class for reading and printing the content of a file.
  */
-function readAndPrintFileContent (filePath) {
-  // Check if the user provided a file path as an argument
-  if (process.argv.length !== 3) {
-    console.error('Usage: ./0-readme.js <file_path>');
-    process.exit(1);
+class FileReader {
+  /**
+   * Create a FileReader instance.
+   *
+   * @param {string} filePath - The path to the file to be read.
+   */
+  constructor (filePath) {
+    /**
+     * The path to the file to be read.
+     * @type {string}
+     */
+    this.filePath = filePath;
   }
 
-  // Read the content of the file in utf-8 encoding
-  fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-      // Print the error object if an error occurred during reading
-      console.error(err);
+  /**
+   * Read and print the content of the file.
+   */
+  readAndPrintFileContent () {
+    // Read the content of the file in utf-8 encoding
+    fs.readFile(this.filePath, 'utf-8', (err, data) => {
+      if (err) {
+        /**
+         * Handle file read error.
+         *
+         * @event FileReader#error
+         * @type {Error}
+         */
+        console.error(err);
+        process.exit(1);
+      }
+
+      /**
+       * Content of the file.
+       *
+       * @event FileReader#data
+       * @type {string}
+       */
+      console.log(data);
+    });
+  }
+
+  /**
+   * Run the FileReader script.
+   * Checks command-line arguments and reads/prints the file content.
+   */
+  static run () {
+    // Check if the user provided a file path as an argument
+    if (process.argv.length !== 3) {
+      /**
+       * Usage error.
+       *
+       * @event FileReader#usageError
+       * @type {Error}
+       */
+      console.error('Usage: ./0-readme.js <file_path>');
       process.exit(1);
     }
 
-    // Print the content of the file
-    console.log(data);
-  });
+    // Get the file path from the command line argument
+    const filePath = process.argv[2];
+
+    // Create an instance of FileReader and read/print the file content
+    const fileReader = new FileReader(filePath);
+    fileReader.readAndPrintFileContent();
+  }
 }
 
-// Get the file path from the command line argument
-const filePath = process.argv[2];
-
-// Call the function to read and print the file content
-readAndPrintFileContent(filePath);
+// Run the script
+FileReader.run();
