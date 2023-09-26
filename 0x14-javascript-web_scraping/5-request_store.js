fb1ee575812a8056fad32a5a949f1b18afc0d2ce
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-09-26 17:12:49 +0100

    dyrane is awesome

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,41 +3,89 @@ const request = require('request');
 const fs = require('fs');
 
 /**
- * Get the contents of a webpage and store it in a file.
- *
- * @param {string} url - The URL of the webpage to request.
- * @param {string} file - The file path to store the body response.
+ * WebpageDownloader class for getting and storing the contents of a webpage.
  */
-function requestAndStoreWebpage (url, file) {
-  // Send a GET request to the provided URL
-  request(url, (error, response, body) => {
-    if (error) {
-      // Print the error object if there was an error with the request
-      console.error(error);
+class WebpageDownloader {
+  /**
+   * Create a WebpageDownloader instance.
+   *
+   * @param {string} url - The URL of the webpage to request.
+   * @param {string} file - The file path to store the body response.
+   */
+  constructor (url, file) {
+    /**
+     * The URL of the webpage to request.
+     * @type {string}
+     */
+    this.url = url;
+
+    /**
+     * The file path to store the body response.
+     * @type {string}
+     */
+    this.file = file;
+  }
+
+  /**
+   * Request and store the webpage content in a file.
+   */
+  requestAndStoreWebpage () {
+    // Send a GET request to the provided URL
+    request(this.url, (error, response, body) => {
+      if (error) {
+        /**
+         * Handle request error.
+         *
+         * @event WebpageDownloader#error
+         * @type {Error}
+         */
+        console.error(error);
+        process.exit(1);
+      } else {
+        // Write the body response to the specified file
+        fs.writeFile(this.file, body, 'utf8', (err) => {
+          if (err) {
+            /**
+             * Handle write file error.
+             *
+             * @event WebpageDownloader#error
+             * @type {Error}
+             */
+            console.error(err);
+            process.exit(1);
+          }
+          // console.log(`Downloaded and saved to ${this.file}`);
+        });
+      }
+    });
+  }
+
+  /**
+   * Run the WebpageDownloader script.
+   * Checks command-line arguments, validates the URL and file path, and downloads the webpage content.
+   */
+  static run () {
+    // Check if the user provided the URL and file path as arguments
+    if (process.argv.length !== 4) {
+      /**
+       * Usage error.
+       *
+       * @event WebpageDownloader#usageError
+       * @type {Error}
+       */
+      console.error('Usage: ./5-request_store.js <url> <file_path>');
       process.exit(1);
-    } else {
-      // Write the body response to the specified file
-      fs.writeFile(file, body, 'utf8', (err) => {
-        if (err) {
-          // Print the error object if there was an error while writing
-          console.error(err);
-          process.exit(1);
-        }
-        // console.log(`Downloaded and saved to ${file}`);
-      });
     }
-  });
+
+    // Define the URL and file path
+    const url = process.argv[2];
+    const file = process.argv[3];
+
+    // Create an instance of WebpageDownloader and request the webpage content
+    const downloader = new WebpageDownloader(url, file);
+    downloader.requestAndStoreWebpage();
+  }
 }
 
-// Check if the user provided the URL and file path as arguments
-if (process.argv.length !== 4) {
-  console.error('Usage: ./5-request_store.js <url> <file_path>');
-  process.exit(1);
-}
-
-// Define the URL and file path
-const url = process.argv[2];
-const file = process.argv[3];
-
-// Call the function to request and store the webpage
-requestAndStoreWebpage(url, file);
+// Run the script
+WebpageDownloader.run();
